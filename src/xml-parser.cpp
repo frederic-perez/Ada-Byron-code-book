805acba4051d89493c5f062225148b2d31ddbe30
Frederic Perez <fredericpcx@gmail.com>
2015-03-25 15:50:27 +0100

    ENH: Cleaning xml-parser.* + input.xml (finished)

--- src/xml-parser.cpp
+++ src/xml-parser.cpp
@@ -8,13 +8,82 @@
 // This demonstrates how to use Boost.PropertyTree
 // to read and write XML files.
 
+#undef XML_DATE_TRANSLATOR_20150325
+
 #include <iostream>
+#ifdef XML_DATE_TRANSLATOR_20150325
+#include <locale>
+#endif
+#include <vector>
+
+//#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/xml_parser.hpp>
+#ifdef XML_DATE_TRANSLATOR_20150325
+#include <boost/date_time/gregorian/gregorian.hpp>
+#endif
 
 #include "aux-raw.h"
 #include "XML-parser.h"
 
-//#include <boost/property_tree/ptree.hpp>
-#include <boost/property_tree/xml_parser.hpp>
+#ifdef XML_DATE_TRANSLATOR_20150325
+
+class DateTranslator
+{
+	typedef boost::date_time::date_facet<Date, char> tOFacet;
+	typedef boost::date_time::date_input_facet<Date, char> tIFacet;
+	std::locale locale_;
+
+	static std::locale isoDateLocale() {
+		std::locale loc = std::locale(loc, new tIFacet("%Y-%m-%d"));
+		loc = std::locale(loc, new tOFacet("%Y-%m-%d"));
+		return loc;
+	}
+
+public:
+	typedef std::string internal_type;
+	typedef Date external_type;
+
+	DateTranslator() : locale_(isoDateLocale()) {}
+
+	boost::optional<external_type>
+		get_value(internal_type const& v)
+	{
+		std::istringstream stream(v);
+		stream.imbue(locale_);
+		external_type vAns;
+		if (stream >> vAns)
+			return vAns;
+		else
+			return boost::none;
+	}
+
+	boost::optional<internal_type>
+		put_value(external_type const& v)
+	{
+		std::ostringstream ans;
+		ans.imbue(locale_);
+		ans << v;
+		return ans.str();
+	}
+};
+
+#endif
+
+#ifdef XML_DATE_TRANSLATOR_20150325
+typedef boost::gregorian::date Date;
+#else
+typedef std::string Date;
+#endif
+
+struct Flight
+{
+	std::string carrier;
+	unsigned number;
+	Date date;
+	bool cancelled;
+};
+
+typedef std::vector<Flight> Flights;
 
 #ifdef XML_DATE_TRANSLATOR_20150325
 
@@ -32,46 +101,45 @@ template<> struct translator_between<std::string, Date> {
 
 namespace {
 
-Flights
-read(std::istream& is)
+void
+read(std::istream& a_is, Flights& a_flights)
 {
 	// populate tree structure pt
 	using boost::property_tree::ptree;
 	ptree pt;
-	read_xml(is, pt);
+	read_xml(a_is, pt);
 	 
 	// traverse pt
-	Flights ans;
-	for (const ptree::value_type& v : pt.get_child("sked")) {
+	for (const ptree::value_type& v : pt.get_child("flights")) 
+	{
 		if (v.first == "flight") {
 			Flight f;
 			f.carrier = v.second.get<std::string>("carrier");
 			f.number = v.second.get<unsigned>("number");
 			f.date = v.second.get<Date>("date");
 			f.cancelled = v.second.get("<xmlattr>.cancelled", false);
-			ans.push_back(f);
+			a_flights.push_back(f);
 		}
 	}
-	return ans;
 }
  
 void
-write(Flights sked, std::ostream & os)
+write(const Flights& a_flights, std::ostream& a_os)
 {
 	using boost::property_tree::ptree;
 	ptree pt;
 	 
-	pt.add("sked.version", 3);
+	pt.add("flights.version", 3);
 	 
-	for (Flight f : sked) {
-		ptree& node = pt.add("sked.flight", "");
-		node.put("carrier", f.carrier);
-		node.put("number", f.number);
-		node.put("date", f.date);
-		if (f.cancelled)
+	for (const auto& flight : a_flights) {
+		ptree& node = pt.add("flights.flight", "");
+		node.put("carrier", flight.carrier);
+		node.put("number", flight.number);
+		node.put("date", flight.date);
+		if (flight.cancelled)
 			node.put("<xmlattr>.cancelled", true);
 	}
-	write_xml(os, pt);
+	write_xml(a_os, pt);
 }
 
 } // namespace
@@ -84,12 +152,14 @@ Ada_Byron_code_book::ParseXML(
 	std::clog << __func__ << " started..." << std::endl;
 	try {
 		std::ifstream input(a_inputFilename);
-		Flights sked = read(input);
-		std::clog << "  ParseXML: Read(\"" << a_inputFilename << "\") finished"
-			<< std::endl;
+		Flights flights;
+		read(input, flights);
+		const std::string pad = "  ";
+		std::clog << pad << __func__ << ": Read(\"" << a_inputFilename
+			<< "\") finished" << std::endl;
 		std::ofstream output(a_outputFilename);
-		write(sked, output);
-		std::clog << "  ParseXML: Writing of \"" << a_outputFilename
+		write(flights, output);
+		std::clog << pad << __func__ << ": Writing of \"" << a_outputFilename
 			<< "\" finished" << std::endl;
 	} catch (...) {
 		return false;
