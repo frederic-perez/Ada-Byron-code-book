2dedb281a1644e6cc127fcaf25bc54e2a012b2db
Frederic Perez <fredericpcx@gmail.com>
2015-03-26 15:45:38 +0100

    Added GNUGppVersion

--- src/aux-spy.cpp
+++ src/aux-spy.cpp
@@ -11,9 +11,24 @@ ABcb::spy::BoostVersion(std::ostream& a_os)
 {
 	return 
 		a_os
-			<< BOOST_VERSION / 100000 << "."  // major version
-			<< BOOST_VERSION / 100 % 1000 << "."  // minior version
-			<< BOOST_VERSION % 100                // patch level
+			<< BOOST_VERSION / 100000 << "."	// major version
+			<< BOOST_VERSION / 100 % 1000 << "."	// minor version
+			<< BOOST_VERSION % 100	// patch level
+			<< std::flush;
+}
+
+std::ostream&
+ABcb::spy::GNUGppVersion(std::ostream& a_os)
+{
+	return 
+		a_os
+#ifdef MY_GNUGPP_VERSION
+			<< MY_GNUGPP_VERSION / 10000 << "."	// major version
+			<< MY_GNUGPP_VERSION / 100 % 100 << "."	// minor version
+			<< MY_GNUGPP_VERSION % 100	// patch level
+#else
+			<< "[not applicable]"
+#endif
 			<< std::flush;
 }
 
--- src/aux-spy.h
+++ src/aux-spy.h
@@ -10,6 +10,15 @@ namespace spy {
 
 std::ostream& BoostVersion(std::ostream&);
 
+#ifdef __GNUG__
+#define MY_GNUGPP_VERSION (__GNUC__ * 10000 \
+	+ __GNUC_MINOR__ * 100 \
+	+ __GNUC_PATCHLEVEL__)
+// #if MY_GNUGPP_VERSION > 30200 // Test for GCC > 3.2.0
+#endif
+
+std::ostream& GNUGppVersion(std::ostream&);
+
 } // namespace spy
 
 } // namespace Ada_Byron_code_book
