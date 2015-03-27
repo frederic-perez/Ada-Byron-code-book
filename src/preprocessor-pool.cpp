e578cbc51236b37d378f7607a993f05c1b33d956
Frederic Perez <fredericpcx@gmail.com>
2015-03-27 12:45:34 +0100

    ENH: Extended function ListOfPreprocessorDefines

--- src/preprocessor-pool.cpp
+++ src/preprocessor-pool.cpp
@@ -10,18 +10,34 @@ std::ostream&
 ABcb::spy::ListOfPreprocessorDefines(std::ostream& a_os)
 {
 	const std::string pad = "  ";
+	const bool definedClang =
+#ifdef __clang__
+		true;
+#else
+		false;
+#endif
+	const bool definedWIN32 =
+#if defined(WIN32)
+		true;
+#else
+		false;
+#endif
+
 	return
 		a_os
+			<< std::boolalpha
+			<< pad << "defined(__clang__): " << definedClang << '\n'
+			<< pad << "defined(WIN32): " << definedWIN32 << '\n'
 #ifdef __GNUG__
-		<< pad << "__GNUG__ = " << __GNUG__ << '\n'
+			<< pad << "__GNUG__ = " << __GNUG__ << '\n'
 #endif
 #ifdef _MSC_VER
-		<< pad << "_MSC_VER = " << _MSC_VER << '\n'
+			<< pad << "_MSC_VER = " << _MSC_VER << '\n'
 #endif
 #ifdef MY_GNUGPP_VERSION
-		<< pad << "MY_GNUGPP_VERSION = " << MY_GNUGPP_VERSION << '\n'
+			<< pad << "MY_GNUGPP_VERSION = " << MY_GNUGPP_VERSION << '\n'
 #endif
-		<< std::flush;
+			<< std::flush;
 }
 
 // -- eof
