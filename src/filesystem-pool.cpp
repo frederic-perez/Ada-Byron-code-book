7dc13fad443568223b515d9c792e5a16b1d7d2d9
Frederic Perez <fredericpcx@gmail.com>
2015-05-14 13:48:08 +0200

    ENH: Added boost::filesystem::exists

--- src/filesystem-pool.cpp
+++ src/filesystem-pool.cpp
@@ -19,12 +19,27 @@ Ada_Byron_code_book::ExamplesOfFileSystem(const std::string& a_filename)
 
 	const std::string pad = "  ";
 
+	// "Testing" an invented filename
+	const std::string inventedFilename =
+		"invented-filename-to-check-if-it-exists.yuk";
+	std::cout << pad << '\"' << inventedFilename << '\"';
+	if (boost::filesystem::exists(inventedFilename))
+		std::cout << " exists" << std::endl;
+	else 
+		std::cout << " does not exist" << std::endl;
+
 	// "Testing" a_filename
 
-	std::cout << pad << a_filename << " has a file_size of "
+	if (!boost::filesystem::exists(a_filename)) { // Bail out ASAP
+		std::cout << " does not exist" << std::endl;
+		std::clog << __func__ << " aborted." << std::endl;
+		return;
+	}
+
+	std::cout << pad << '\"' << a_filename << '\"' << " has a file_size of "
 		<< file_size(a_filename) << " bytes" << std::endl;
 
-	std::cout << pad << a_filename;
+	std::cout << pad << '\"' << a_filename << '\"';
 	const path myPath1(a_filename);
 	if (myPath1.has_extension())
 		std::cout << " has the extension " << myPath1.extension();
