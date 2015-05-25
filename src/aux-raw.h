01186573d8b9aacf68793ee7e95418021d514dd5
Frederic Perez <fredericpcx@gmail.com>
2015-05-25 15:58:10 +0200

    BUG: Minor missing include bug (found today) fixed

--- src/aux-raw.h
+++ src/aux-raw.h
@@ -10,6 +10,8 @@
 // - Apple OS X Yosemite
 // - Xcode 6.1 with Apple LLVM 6.0 (clang-600.0.54) (based on LLVM 3.5svn)
 
+#include <string>
+
 // Unfortulately __func__ is not understood by vs12. This is a workaround:
 #ifndef __func__
 #define __func__ __FUNCTION__
