dd04872fd8ae16479b54e74f6185297e2058ff59
Frederic Perez <fredericpcx@gmail.com>
2015-05-26 16:02:14 +0200

    ENH: Added miscellaneous examples

--- src/algorithm-string-pool.cpp
+++ src/algorithm-string-pool.cpp
@@ -2,8 +2,10 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
 
 #include <boost/algorithm/string.hpp>
+#include <boost/range/algorithm/remove_if.hpp>
 
 #include "aux-raw.h"
 #include "algorithm-string-pool.h"
@@ -23,21 +25,39 @@ Ada_Byron_code_book::ExamplesOfAlgorithmsString()
 {
 	std::clog << __func__ << " started..." << std::endl;
 	
-	std::string name = "  Alan Turing  ";
-	Output("Name (original)", name);
-	
+	std::string name = "  Alan Turing 42  ";
+	Output("name (original)", name);
+	name.erase(boost::remove_if(name, ::isdigit), name.end());
+	Output("name (after isdigit removal)", name);
+
 	namespace ba = boost::algorithm;
 	ba::to_lower(name);
-	Output("Name (after to_lower)", name);
+	Output("name (after to_lower)", name);
 	ba::to_upper(name);
-	Output("Name (after to_upper)", name);
+	Output("name (after to_upper)", name);
 	ba::trim_right(name);
-	Output("Name (after trim_right)", name);
+	Output("name (after trim_right)", name);
 	ba::trim(name);
-	Output("Name (after trim)", name);
-	name.erase(remove_if(name.begin(), name.end(), isspace), name.end());
-	Output("Name (after isspace removal)", name);
-	
+	Output("name (after trim)", name);
+	name.erase(remove_if(name.begin(), name.end(), ::isspace), name.end());
+	Output("name (after isspace removal)", name);
+	name.erase(
+		remove_if(name.begin(), name.end(), boost::is_any_of("LN")),
+		name.end());
+	Output("name (after is_any_of(\"LN\") removal)", name);
+
+	const std::string line = "aa bb , cc dd , ee , gg,hh, ii ,kk";
+	Output("line (original)", line);
+	// Split the line a into sections using "," as the delimiter,
+	// store result into a vector of strings
+	// Code adapted from http://www.panix.com/~jrr/boost/string.htm on 05.26.2015
+	std::vector<std::string> tokens;
+	boost::split(tokens, line, boost::is_any_of(","));
+	struct Trim { void operator()(std::string& a_s) { boost::trim(a_s); } };
+	for_each(tokens.begin(), tokens.end(), Trim());
+	const std::string instanceCleaned = ba::join(tokens, ",");
+	Output("line (after blanks around commas removal)", instanceCleaned);
+
 	std::clog << __func__ << " finished." << std::endl;
 }
 
