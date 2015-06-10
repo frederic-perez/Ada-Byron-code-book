c1b2452a3bcb1e943ccf1ca2bee80146ad28d0ff
Frederic Perez <fredericpcx@gmail.com>
2015-06-10 15:33:57 +0200

    COMP: Petty enhancement with new macros

--- src/under-construction.h
+++ src/under-construction.h
@@ -10,6 +10,12 @@
 
 #include <boost/preprocessor.hpp>
 
+#define ABcb_TO_STR(unused,data,elem) BOOST_PP_STRINGIZE(elem),
+#define ABcb_DECLARE_ENUM_AND_ENUMTEXT(sequence) \
+enum class Enum : uint8_t { BOOST_PP_SEQ_ENUM(sequence) }; \
+const std::array<const char*, BOOST_PP_SEQ_SIZE(sequence)> enumText{{ \
+	BOOST_PP_SEQ_FOR_EACH(ABcb_TO_STR, ~, sequence) }};
+
 namespace Ada_Byron_code_book {
 
 namespace Guru { // An enum class and related stuff
@@ -18,27 +24,16 @@ namespace Guru { // An enum class and related stuff
 // http://www.gamedev.net/topic/437852-c-enum-names-as-strings/
 
 #define SEQUENCE (undefined)(Alexandrescu)(Koenig)(Meyers)(Stroustrup)(Sutter)
-#define TO_STR(unused,data,elem) BOOST_PP_STRINGIZE(elem),
-enum class Enum : uint8_t { BOOST_PP_SEQ_ENUM(SEQUENCE) };
-const std::array<const char*, BOOST_PP_SEQ_SIZE(SEQUENCE)> enumText{ { 
-	BOOST_PP_SEQ_FOR_EACH(TO_STR, ~, SEQUENCE) } };
+ABcb_DECLARE_ENUM_AND_ENUMTEXT(SEQUENCE)
 Enum GetEnum(const std::string&);
 std::string GetString(Enum);
 std::vector<std::string> GetDefinedStrings();
 
-#undef TO_STR
 #undef SEQUENCE
 
 } // namespace Fruit
 
 void GurusTest(std::initializer_list<std::string>);
-
-/*
-#define PROJECT_PREFIX_DO_EVIL_TO_STR(unused,data,elem) BOOST_PP_STRINGIZE(elem),
-#define PROJECT_PREFIX_DO_EVIL(enum_,strings,elements) \ 
-enum enum_ { BOOST_PP_SEQ_ENUM(elements) };                 \
-	const char * strings[] = { BOOST_PP_SEQ_FOR_EACH(PROJECT_PREFIX_DO_EVIL_TO_STR, ~, SEQ) };
-*/
 
 } // namespace Ada_Byron_code_book
 
