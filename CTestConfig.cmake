3ff059fc3146ff38032e03dec57917531450c508
Frederic Perez <fredericpcx@gmail.com>
2015-09-09 18:47:52 +0200

    ENH: Added "dashboard" functionality.
    Succeeded for vs14 (Windows), g++482 (ubuntu), with the newly (free)
    created dashboard my.cdash.org/index.php?project=Ada-Byron-code-book

---
+++ CTestConfig.cmake
@@ -0,0 +1,11 @@
+# -- 
+
+project(Ada-Byron-code-book)
+
+set(CTEST_PROJECT_NAME "${PROJECT_NAME}")
+set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_SITE "my.cdash.org")
+set(CTEST_DROP_LOCATION "/submit.php?project=${PROJECT_NAME}")
+set(CTEST_DROP_SITE_CDASH TRUE)
+
+# -- eof
