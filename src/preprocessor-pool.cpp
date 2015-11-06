034d53fb34ea8ef22c6945b9318caa8a61e8b879
Frederic Perez <fredericpcx@gmail.com>
2015-11-06 11:03:40 +0100

    ENH: Added Mac-related preprocessor defines

--- src/preprocessor-pool.cpp
+++ src/preprocessor-pool.cpp
@@ -35,6 +35,24 @@ ABcb::spy::ListOfPreprocessorDefines(std::ostream& a_os)
 #else
 		false;
 #endif
+	const bool defined_macintosh =
+#if defined(macintosh)
+		true;
+#else
+		false;
+#endif
+	const bool defined_APPLE =
+#if defined(__APPLE__)
+		true;
+#else
+		false;
+#endif
+	const bool defined_APPLE_CC =
+#if defined(__APPLE_CC__)
+		true;
+#else
+		false;
+#endif
 	const bool definedNDEBUG =
 #if defined(NDEBUG)
 	true;
@@ -46,9 +64,12 @@ ABcb::spy::ListOfPreprocessorDefines(std::ostream& a_os)
 		a_os
 			<< std::boolalpha
 			<< pad << "defined(__clang__): " << definedClang << '\n'
+			<< pad << "defined(WIN32): " << definedWIN32 << '\n'
 			<< pad << "defined(_WIN32): " << defined_WIN32 << '\n'
 			<< pad << "defined(_WIN64): " << defined_WIN64 << '\n'
-			<< pad << "defined(WIN32): " << definedWIN32 << '\n'
+			<< pad << "defined(macintosh): " << defined_macintosh << '\n'
+			<< pad << "defined(__APPLE__): " << defined_APPLE << '\n'
+			<< pad << "defined(__APPLE_CC__): " << defined_APPLE_CC << '\n'
 			<< pad << "defined(NDEBUG): " << definedNDEBUG << '\n'
 #ifdef __GNUG__
 			<< pad << "__GNUG__ = " << __GNUG__ << '\n'
