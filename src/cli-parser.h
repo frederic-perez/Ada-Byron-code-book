c4ab17fc02c629af9992a0450badb1f8a82f6f31
Frederic Perez <fredericpcx@gmail.com>
2015-11-11 16:26:23 +0100

    COMP: Refactoring namespace+enum stuff (WIP#2)

--- src/cli-parser.h
+++ src/cli-parser.h
@@ -13,69 +13,23 @@
 
 namespace Ada_Byron_code_book {
 
-namespace PlatonicSolid { // An enum class and related stuff
+ABcb_DEFINE_NAMESPACE_WITH_ENUM_TOOLS(\
+	PlatonicSolid,
+	(undefined)\
+	(tetrahedron)(octahedron)(icosahedron)(hexahedron)(dodecahedron)
+)
 
-enum class Enum : uint8_t {
-	undefined,
-	tetrahedron, octahedron, icosahedron, hexahedron, dodecahedron
-};
-const std::array<const char*, 6> enumText{ {
-	"undefined",
-	"tetrahedron", "octahedron", "icosahedron", "hexahedron", "dodecahedron" } };
-Enum GetEnum(const std::string&);
-std::string GetString(Enum);
-std::vector<std::string> GetDefinedStrings();
-
-} // namespace PlatonicSolid
-
-namespace Color { // An enum class and related stuff
-
-enum class Enum : uint8_t {
-	undefined,
-	red, green, blue
-};
-const std::array<const char*, 4> enumText{ {
-	"undefined",
-	"red", "green", "blue" } };
-Enum GetEnum(const std::string&);
-std::string GetString(Enum);
-std::vector<std::string> GetDefinedStrings();
-
-} // namespace Color
-
-namespace Kruit { // An enum class and related stuff
-
-// Code originarily based on
-// http://www.gamedev.net/topic/437852-c-enum-names-as-strings/
-
-#define SEQUENCE (undefined)(apple)(orange)(pear)
-#define TO_STR(unused, data, elem) BOOST_PP_STRINGIZE(elem),
-enum class Enum : uint8_t { BOOST_PP_SEQ_ENUM(SEQUENCE) };
-const std::array<const char*, BOOST_PP_SEQ_SIZE(SEQUENCE)> enumText{ { 
-	BOOST_PP_SEQ_FOR_EACH(TO_STR, ~, SEQUENCE) } };
-Enum GetEnum(const std::string&);
-std::string GetString(Enum);
-std::vector<std::string> GetDefinedStrings();
-
-#undef TO_STR
-#undef SEQUENCE
-
-} // namespace Kruit
+ABcb_DEFINE_NAMESPACE_WITH_ENUM_TOOLS(\
+	Color,
+	(undefined)\
+	(red)(green)(blue)
+)
 
 ABcb_DEFINE_NAMESPACE_WITH_ENUM_TOOLS(\
 	Fruit,
 	(undefined)\
 	(apple)(orange)(pear)
 )
-
-/*
-#define PROJECT_PREFIX_DO_EVIL_TO_STR(unused, data, elem) \
-	BOOST_PP_STRINGIZE(elem),
-#define PROJECT_PREFIX_DO_EVIL(enum_, strings, elements) \ 
-enum class enum_ { BOOST_PP_SEQ_ENUM(elements) };                 \
-	const char * strings[] = \
-		{ BOOST_PP_SEQ_FOR_EACH(PROJECT_PREFIX_DO_EVIL_TO_STR, ~, SEQ) };
-*/
 
 namespace cli {
 
--- src/under-construction.h
+++ src/under-construction.h
@@ -13,13 +13,10 @@
 // TODO: (iss) Parse point cloud (ASCII) file as a 1st example (3DI)
 
 #define ABcb_TO_STR(unused, data, elem) BOOST_PP_STRINGIZE(elem),
-/*
-#define ABcb_DECLARE_ENUM_AND_ENUMTEXT(sequence) \
-enum class Enum : uint8_t { BOOST_PP_SEQ_ENUM(sequence) }; \
-const std::array<const char*, BOOST_PP_SEQ_SIZE(sequence)> enumText{{ \
-	BOOST_PP_SEQ_FOR_EACH(ABcb_TO_STR, ~, sequence) }};
-*/
 
+// Code originarily based on
+// http://www.gamedev.net/topic/437852-c-enum-names-as-strings/
+//
 #define ABcb_DEFINE_NAMESPACE_WITH_ENUM_TOOLS(namespaceName, sequence) \
 namespace namespaceName { \
 \
@@ -62,20 +59,12 @@ GetDefinedStrings() \
 
 namespace Ada_Byron_code_book {
 
-//namespace Guru { // An enum class and related stuff
-
-// Code originarily based on
-// http://www.gamedev.net/topic/437852-c-enum-names-as-strings/
-
-//ABcb_DECLARE_ENUM_AND_ENUMTEXT(
 ABcb_DEFINE_NAMESPACE_WITH_ENUM_TOOLS(\
 	Guru,
 	(undefined)\
 	(Andrei_Alexandrescu)(Andrew_Koenig)(Bruce_Eckel)(Bjarne_Stroustrup)\
 	(Herb_Sutter)(Ira_Pohl)(Scott_Meyers)
 )
-
-//} // namespace Guru
 
 void GurusTest(std::initializer_list<std::string>);
 
