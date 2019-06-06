95fe64c3bd1a4fed7684935bedfe5837959c5836
Frederic Perez <frederic.perez@gmx.us>
2019-06-06 11:29:50 +0200

    master :lipstick: Petty changes

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,17 +1,13 @@
-# -- 
-
 project(Ada-Byron-code-book)
 
 set(CTEST_PROJECT_NAME "${PROJECT_NAME}")
 set(CTEST_DROP_METHOD "http")
 option(USE_MY_LOCAL_CDASH "Use my local CDash" OFF)
 if(USE_MY_LOCAL_CDASH)
-	set(CTEST_DROP_SITE "172.26.0.117")
-	set(CTEST_DROP_LOCATION "/CDash/submit.php?project=${PROJECT_NAME}")
+  set(CTEST_DROP_SITE "172.26.0.117")
+  set(CTEST_DROP_LOCATION "/CDash/submit.php?project=${PROJECT_NAME}")
 else()
-	set(CTEST_DROP_SITE "my.cdash.org")
-	set(CTEST_DROP_LOCATION "/submit.php?project=${PROJECT_NAME}")
+  set(CTEST_DROP_SITE "my.cdash.org")
+  set(CTEST_DROP_LOCATION "/submit.php?project=${PROJECT_NAME}")
 endif()
 set(CTEST_DROP_SITE_CDASH TRUE)
-
-# -- eof
